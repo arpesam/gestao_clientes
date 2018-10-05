4d6deb712a937eb1f350f22a015acf28e4bc2332
Danilo <daniloarpesam@gmail.com>
2018-10-05 10:41:27 -0300

    Relation in db added

--- clientes/admin.py
+++ clientes/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Person
+from .models import Person, Documento, Venda, Produto
 
 admin.site.register(Person)
+admin.site.register(Documento)
+admin.site.register(Venda)
+admin.site.register(Produto)
